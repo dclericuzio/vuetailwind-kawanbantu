99fa01fe7f4ed187cec7cb63a393c29b8808b066
dante clericuzio <danteclericuzio@dantes-MacBook-Pro.local>
2022-10-12 14:13:30 +0700

    name fix

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,67 +3,120 @@
     <div class="pt-12 pb-24 md:pl-24 pl-52 bg-[#181D29] justify-items-center">
       <div class="md:flex-row flex flex-col justify-around text-center">
         <div>
-          <img class="py-10" src="../assets/logo.png" alt="logo.footer">
-          <h1 class="text-white w-80 text-start">QP office, tanjung mas raya blok B1 no.44, Jagakarsa, Jakarta Selatan 12530</h1>
+          <img class="py-10" src="../assets/logo.png" alt="logo.footer" />
+          <h1 class="text-white w-80 text-start">
+            QP office, tanjung mas raya blok B1 no.44, Jagakarsa, Jakarta
+            Selatan 12530
+          </h1>
           <div class="flex flex-row gap-x-7 py-8">
-            <img src="../assets/icons/Youtube.svg" alt="youtube.icon">
-            <img src="../assets/icons/Tiktok.svg" alt="tikktok.icon">
-            <img src="../assets/icons/Instagram.svg" alt="instagram.icon">
+            <img src="../assets/icons/Youtube.svg" alt="youtube.icon" />
+            <img src="../assets/icons/Tiktok.svg" alt="tikktok.icon" />
+            <img src="../assets/icons/Instagram.svg" alt="instagram.icon" />
           </div>
         </div>
 
-        <div class="justify-center align-middle pt-24 ">
-          <div class="text-start"> 
-            <h1 class="text-[18px] font-bold text-white tracking-widest w-[400px] md:pl-24 pb-4">Seputar KawanBantu</h1>
-            <div class="md:mx-24">
-              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">Beranda</h1>
-              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">Fitur</h1>
-              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">Tentang Kami</h1>
-              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">Contact Us</h1>
-            </div>
-          </div>
-        </div>
-        
         <div class="justify-center align-middle pt-24">
-          <div class="text-start"> 
-            <h1 class="text-[18px] font-bold text-white tracking-widest md:pl-24 pb-4">Kontak Kami</h1>
+          <div class="text-start">
+            <h1
+              class="text-[18px] font-bold text-white tracking-widest w-[400px] md:pl-24 pb-4"
+            >
+              Seputar KawanBantu
+            </h1>
             <div class="md:mx-24">
-              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">+628231264835</h1>
-              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">kawanbantu@gmail.com</h1>
+              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">
+                Beranda
+              </h1>
+              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">
+                Fitur
+              </h1>
+              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">
+                Tentang Kami
+              </h1>
+              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">
+                Contact Us
+              </h1>
             </div>
           </div>
         </div>
-        
+
+        <div class="justify-center align-middle pt-24">
+          <div class="text-start">
+            <h1
+              class="text-[18px] font-bold text-white tracking-widest md:pl-24 pb-4"
+            >
+              Kontak Kami
+            </h1>
+            <div class="md:mx-24">
+              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">
+                +628231264835
+              </h1>
+              <h1 class="text-[14px] font-sans text-white tracking-wider py-2">
+                kawanbantu@gmail.com
+              </h1>
+            </div>
+          </div>
+        </div>
+
         <div class="md:ml-28">
           <div class="justify-center align-middle pt-24">
-          <div class="text-start"> 
-            <h1 class="text-[18px] font-bold text-white tracking-widest pb-4">Platform Berdonasi</h1>
-            <div class="ml-0 flex flex-row flex-wrap w-[50%]">
-                <img class="mx-2 py-1" src="../assets/icons/Gopay.svg" alt="gopay.icon">
-                <img class="mx-2 py-1" src="../assets/icons/Shopeepay.svg" alt="shopeepay.icon">
-                <img class="mx-2 py-1" src="../assets/icons/Bank BCA Logo (SVG-240p) - FileVector69 1.svg" alt="bca.icon">
-                <img class="mx-2 py-1" src="../assets/icons/Bank Mandiri Logo (SVG-240p) - FileVector69 1.svg" alt="bank_mandiri.icon">
-                <img class="mx-2 py-1" src="../assets/icons/BNI.svg" alt="bni.icon">
-                <img class="mx-2 py-1" src="../assets/icons/BANK_BRI_logo 1.svg" alt="bri.icon">
-                <img class="mx-2 py-1" src="../assets/icons/Permata Bank 1.svg" alt="permata_bank.icon">
+            <div class="text-start">
+              <h1 class="text-[18px] font-bold text-white tracking-widest pb-4">
+                Platform Berdonasi
+              </h1>
+              <div class="ml-0 flex flex-row flex-wrap w-[50%]">
+                <img
+                  class="mx-2 py-1"
+                  src="../assets/icons/Gopay.svg"
+                  alt="gopay.icon"
+                />
+                <img
+                  class="mx-2 py-1"
+                  src="../assets/icons/Shopeepay.svg"
+                  alt="shopeepay.icon"
+                />
+                <img
+                  class="mx-2 py-1"
+                  src="../assets/icons/bca.svg"
+                  alt="bca.icon"
+                />
+                <img
+                  class="mx-2 py-1"
+                  src="../assets/icons/mandiri.svg"
+                  alt="bank_mandiri.icon"
+                />
+                <img
+                  class="mx-2 py-1"
+                  src="../assets/icons/BNI.svg"
+                  alt="bni.icon"
+                />
+                <img
+                  class="mx-2 py-1"
+                  src="../assets/icons/bri.svg"
+                  alt="bri.icon"
+                />
+                <img
+                  class="mx-2 py-1"
+                  src="../assets/icons/permata.svg"
+                  alt="permata_bank.icon"
+                />
+              </div>
             </div>
           </div>
         </div>
-        </div>    
       </div>
       <div class="relative">
-        <h1 class="absolute right-[55px] text-lg text-white md:text-center w-full md:py-8 pt-11">© Copyright 2022 KawanBantu. All rights reserved</h1>
+        <h1
+          class="absolute right-[55px] text-lg text-white md:text-center w-full md:py-8 pt-11"
+        >
+          © Copyright 2022 KawanBantu. All rights reserved
+        </h1>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
